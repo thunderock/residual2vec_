5b018e4d4eee83876ea7e17071899fba6f29d6fd
Ashutosh Tiwari <ashutiwa@iu.edu>
2022-12-25 20:33:50 -0500

    all done need to run for pokec

--- train_word2vec_without_dw_nv.py
+++ train_word2vec_without_dw_nv.py
@@ -4,10 +4,9 @@ from node2vec import node2vecs
 
 
 device = "cuda:0"
-numbers = ['one']# , 'two', 'three', 'four', 'five']
+numbers = ['one' , 'two', 'three', 'four', 'five']
 dim = 128
-datasets = ['airport', 'polbook', 'polblog', 'pokec']
-datasets = ['airport']
+datasets = ['airport', 'polbook', 'polblog']
 method = "word2vec"
 
 for dataset in datasets:
@@ -16,8 +15,8 @@ for dataset in datasets:
     for idx, adj_file_path in enumerate(adj_file_paths):
         number = numbers[idx]
         adj_mat = snakemake_utils.get_adj_mat_from_path(adj_file_path)
-        # convert this to csr format
         adj_mat = adj_mat.tocsr()
+        print(dataset, number, adj_mat.shape)
         
         noise_sampler = node2vecs.utils.node_sampler.ConfigModelNodeSampler()
         embs = graph_utils.generate_embedding_with_word2vec(adj_mat, dim, noise_sampler, device)
