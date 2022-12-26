34945729297203cb43df75db6d6145a7dd00b45f
Ashutosh Tiwari <ashutiwa@iu.edu>
2022-12-25 20:09:35 -0500

    added ad hoc script to train word2vec

---
+++ train_word2vec_without_dw_nv.py
@@ -0,0 +1,23 @@
+import numpy as np
+from utils import snakemake_utils, graph_utils
+from node2vec import node2vecs
+
+
+device = "cuda:0"
+numbers = ['one', 'two', 'three', 'four', 'five']
+dim = 128
+datasets = ['airport', 'polbook', 'polblog', 'pokec']
+datasets = ['airport']
+method = "word2vec"
+
+for dataset in datasets:
+    adj_file_paths = ['../final_crosswalk/{}/{}_{}/{}_adj.npz'.format(dataset, dataset, number, dataset) for number in numbers]
+
+    for idx, adj_file_path in enumerate(adj_file_paths):
+        number = numbers[idx]
+        adj_mat = snakemake_utils.get_adj_mat_from_path(adj_file_path)
+        
+        noise_sampler = node2vecs.utils.ConfigModelNodeSampler()
+        embs = graph_utils.generate_embedding_with_word2vec(adj_mat, dim, noise_sampler, device)
+        
+        np.save('../final_crosswalk/{}/{}_{}/{}_{}_{}_embs.npy'.format(dataset, dataset, number, dataset, method, dim), embs)
