a06f22531956e8e6bf4f0ae284717cc3806526f3
Ashutosh Tiwari <ashutiwa@iu.edu>
2023-02-01 15:59:23 -0500

    added faiss cpu

---
+++ models/fast_knn_cpu.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# @Author: Ashutosh Tiwari
+# @Email: checkashu@gmail.com
+# @Date:   2023-02-01 13:36:12
+# @Filepath: models/fast_knn_cpu.py
+import numpy as np
+from scipy import sparse
+import faiss
+
+
+class FastKnnCpu(object):
+    
+    def __init__(self, k=1, metric="euclidean", exact=False, nprobe=50, min_cluster_size=10000) -> None:
+        
+        # exact can be True or False. If None, then it is automatically set to True if n_samples < 1000
+        self.k = k
+        auto = "AUTO"
+        self.metric = metric
+        self.exact = exact
+        self.nprobe = nprobe
+        self.min_cluster_size = min_cluster_size
+    
+    def fit(self, X):
+        n_samples, n_features = X.shape[0], X.shape[1]
+        X = X.astype("float32")
+        if self.exact is None:
+            if n_samples < 1000:
+                self.exact = True
+            else:
+                self.exact = False
+        index = (
+        faiss.IndexFlatL2(n_features)
+        if self.metric == "euclidean"
+        else faiss.IndexFlatIP(n_features)
+        )
+        if not self.exact:
+            nlist = np.maximum(int(n_samples / self.min_cluster_size), 2)
+            faiss_metric = (
+                faiss.METRIC_L2 if self.metric == "euclidean" else faiss.METRIC_INNER_PRODUCT
+            )
+            index = faiss.IndexIVFFlat(index, n_features, int(nlist), faiss_metric)
+        if not index.is_trained:
+            Xtrain = X[
+                np.random.choice(
+                    X.shape[0],
+                    np.minimum(X.shape[0], self.min_cluster_size * 5),
+                    replace=False,
+                ),
+                :,
+            ].copy(order="C")
+            index.train(Xtrain)
+        index.add(X)
+        index.nprobe = self.nprobe
+        self.index = index
+        return self
+    
+    def predict(self, X):
+        X = X.astype("float32")
+        D, I = self.index.search(X, k=self.k)
+        return I
+    
